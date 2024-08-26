7c40dc05a189a1a7d8b33ab0a2ef93f54382f5f3
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-08-27 02:16:04 +0330

    Update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser
+
+class AdminView(admin.ModelAdmin):
+    list_display = ('username','first_name','last_name','email')
+    list_display_links = ('id','username','first_name','last_name','email')
+    search_fields = ('id','username')
 
 # Register your models here.
+admin.site.register(CustomUser)
